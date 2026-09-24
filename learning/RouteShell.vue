87b5f1f63c7132d59d231eb6e57c7dfef4c7cabb
gcake119 <96661305+gcake119@users.noreply.github.com>
2026-09-25 05:22:53 +0800

    feat: add interactive token profiler deck

---
+++ learning/RouteShell.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import App from './App.vue';
+import TokenProfilerDeck from './decks/token-profiler/TokenProfilerDeck.vue';
+const isDeck = ref(location.hash === '#/deck/token-profiler');
+function sync() { isDeck.value = location.hash === '#/deck/token-profiler'; }
+onMounted(() => window.addEventListener('hashchange', sync));
+onUnmounted(() => window.removeEventListener('hashchange', sync));
+</script>
+<template><TokenProfilerDeck v-if="isDeck" /><App v-else /></template>
