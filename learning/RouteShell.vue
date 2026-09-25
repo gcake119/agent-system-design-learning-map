935e8e254a9ea1ff30ca06df5fb2b23197144703
gcake119 <96661305+gcake119@users.noreply.github.com>
2026-09-25 23:17:46 +0800

    feat: route v2 learning map separately

--- learning/RouteShell.vue
+++ learning/RouteShell.vue
@@ -2,9 +2,12 @@
 import { ref, onMounted, onUnmounted } from 'vue';
 import App from './App.vue';
 import TokenProfilerDeck from './decks/token-profiler/TokenProfilerDeck.vue';
+import LearningMapV2 from './v2/LearningMapV2.vue';
+import './v2/v2.css';
 const isDeck = ref(location.hash === '#/deck/token-profiler');
-function sync() { isDeck.value = location.hash === '#/deck/token-profiler'; }
+const isV2 = ref(location.hash.startsWith('#/v2'));
+function sync() { isDeck.value = location.hash === '#/deck/token-profiler'; isV2.value = location.hash.startsWith('#/v2'); }
 onMounted(() => window.addEventListener('hashchange', sync));
 onUnmounted(() => window.removeEventListener('hashchange', sync));
 </script>
-<template><TokenProfilerDeck v-if="isDeck" /><App v-else /></template>
+<template><TokenProfilerDeck v-if="isDeck" /><LearningMapV2 v-else-if="isV2" /><App v-else /></template>
