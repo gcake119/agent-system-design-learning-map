d1a0cdb8522764f8d9e76f8ad5b4d5f0b13bf835
gcake119 <96661305+gcake119@users.noreply.github.com>
2026-09-26 13:25:07 +0800

    refactor: use unversioned learning map entry

--- learning/main.js
+++ learning/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import LearningMapV2 from './v2/LearningMapV2.vue'
-import './v2/v2.css'
+import LearningMap from './LearningMap.vue'
+import './style.css'
 
-createApp(LearningMapV2).mount('#app')
+createApp(LearningMap).mount('#app')
